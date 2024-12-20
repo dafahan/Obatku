67b58b63f52ffa18b9f097e2fe95e91f80fdd91c
dafahan <dafahanid@gmail.com>
2024-12-20 12:13:33 +0700

    initial commit

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/dafahan/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/dafahan/code/mobile/flutter/drugstoretotallylegal/drugstore" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/dafahan/flutter"
+  "PROJECT_DIR=/home/dafahan/code/mobile/flutter/drugstoretotallylegal/drugstore"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8xYWM2MTFjNjRlYWRiZDkzYzVmNWFiYTU0OTRiOGZjM2IzNWVlOTUyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/dafahan/code/mobile/flutter/drugstoretotallylegal/drugstore/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/dafahan/code/mobile/flutter/drugstoretotallylegal/drugstore/lib/main.dart"
+)
